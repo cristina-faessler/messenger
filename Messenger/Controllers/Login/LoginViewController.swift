9fca77d1d10077a8e3b68c6c80d97da7bfa76873
Cristina Bolohan <cristina.bolohan@irt.de>
2020-12-02 17:18:08 +0100

    connect to Firebase

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -123,13 +123,17 @@ class LoginViewController: UIViewController {
             return
         }
         //Firebase Log In
-        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {authResult, error in
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {[weak self] authResult, error in
+            guard let strongSelf = self else {
+                return
+            }
             guard let result = authResult, error == nil else {
                 print("Failed to log in user with email: \(email)")
                 return
             }
             let user = result.user
             print("Logged in user: \(user)")
+            strongSelf.navigationController?.dismiss(animated: true, completion: nil)
         })
         
     }
